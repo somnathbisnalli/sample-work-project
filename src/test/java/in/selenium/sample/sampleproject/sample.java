9ead88c470100d3f95fdba7daaa51ea4d6463f9b
somnath <somnath.bisnalli@gmail.com>
2022-03-12 22:03:36 +0530

    sampleproject

---
+++ src/test/java/in/selenium/sample/sampleproject/sample.java
@@ -0,0 +1,11 @@
+package in.selenium.sample.sampleproject;
+
+import org.testng.annotations.Test;
+
+public class sample {
+
+@Test
+  public void testcase() {
+	System.out.println("this is a sample code");
+}
+}
